1634f88bd06a61516b113a342f9f7933871a4981
hangelici <henrique.angelici@ourosafra.com.br>
2025-12-09 14:08:24 -0300

    Ticket 940874 - Ajuste de Datas de Limite e Mov. Saldo

--- Cereais/OS_INSEREPEDCTR.sql
+++ Cereais/OS_INSEREPEDCTR.sql
@@ -43,6 +43,8 @@ v_COD_PRODUTOR NUMBER;
 v_NOME_PRODUTOR VARCHAR2(100);
 v_CNPJF_PRODUTOR VARCHAR2(20);
 v_INSCESTAD_PRODUTOR VARCHAR2(20);
+v_dtlimite DATE;
+v_dtmov DATE;
 
 BEGIN
 
@@ -101,17 +103,35 @@ BEGIN
     from pedcab_u
    where pedcab_u.estab=:NEW.ESTAB and pedcab_u.serie=:NEW.SERIE and pedcab_u.numero=:NEW.NUMERO; 
    
+    BEGIN
+    IF  :NEW.DTPREVISAO < TRUNC(CURRENT_DATE)
+    THEN
+        v_dtmov := TRUNC(CURRENT_DATE);
+    ELSE
+        v_dtmov := :NEW.DTPREVISAO;
+    END IF;
+    END;
+
+    BEGIN
+    IF :NEW.DTVALIDADE < TRUNC(CURRENT_DATE)
+    THEN
+        v_dtlimite := TRUNC(CURRENT_DATE);
+    ELSE
+        v_dtlimite := :NEW.DTVALIDADE;
+    END IF;
+    END;
+   
    INSERT INTO CONTRATO(contrato.estab,contrato.contrato,contrato.numintermediario,contrato.endalternativo,contrato.contconf,contrato.numerocm,contrato.numerocmadic,contrato.dtemissao,contrato.dtvencto,contrato.userid,
    contrato.safra,contrato.valorprod,contrato.valortotal,contrato.moeda,contrato.observacoes,contrato.prioridade,contrato.padrao,contrato.saldovalor,contrato.ativo,contrato.datalimiteent,contrato.datalimiteliq,
    contrato.dtmovsaldo,contrato.dtlimentimp,contrato.dtlimliqimp,contrato.dtcotacao,contrato.tiporateio,vlrfrete,moedadia)
 
    VALUES(:NEW.ESTAB,CONTRATOD,:NEW.NUMERO,:NEW.SEQENDERECO,CONTRATOCONF,:NEW.PESSOA,:NEW.PESSOA,to_date(CURRENT_DATE),COALESCE(v_prazo,prazopagamento),:new.userid,
    :new.safra,:new.VALORMERCADORIA,:new.VALORMERCADORIA,v_moeda,:NEW.OBS,1,'N',:new.valortotal,'A',:new.DTPREVISAO,:new.DTVALIDADE,
-   :new.DTPREVISAO,:new.DTVALIDADE,:new.DTVALIDADE,to_date(CURRENT_DATE),0,:new.kmfrete,v_moeda_data);
+   v_dtmov,v_dtlimite,v_dtlimite,to_date(CURRENT_DATE),0,:new.kmfrete,v_moeda_data);
 
    INSERT INTO CONTRATOITE (CONTRATOITE.ESTAB,CONTRATOITE.CONTRATO,CONTRATOITE.SEQITEM,CONTRATOITE.ITEM,CONTRATOITE.LOCAL,CONTRATOITE.DTEMISSAO,CONTRATOITE.QUANTIDADE,CONTRATOITE.VALORUNIT,
    CONTRATOITE.VALORTOTAL,CONTRATOITE.TIPOSALDO,CONTRATOITE.PRECOVENDA,CONTRATOITE.QUALCOTACAO,CONTRATOITE.DTMOVSALDO)
-   VALUES (:NEW.ESTAB,CONTRATOD,v_seqitem/*ITEM*/,ITEM,PEDLOCAL,to_date(CURRENT_DATE),QUANTIDADE,ARREDONDAR((valorunitario*V_MULTIPLIC),6),ARREDONDAR((VALOR),2),'A','I','N',:new.DTPREVISAO);
+   VALUES (:NEW.ESTAB,CONTRATOD,v_seqitem/*ITEM*/,ITEM,PEDLOCAL,to_date(CURRENT_DATE),QUANTIDADE,ARREDONDAR((valorunitario*V_MULTIPLIC),6),ARREDONDAR((VALOR),2),'A','I','N',v_dtmov);
 
    INSERT INTO CONTRATOESTAB (ESTAB,CONTRATO,ESTABBX)
    VALUES (:NEW.ESTAB,CONTRATOD,:NEW.ESTAB);
@@ -155,7 +175,7 @@ BEGIN
    VALUES (:NEW.ESTAB,CONTRATOD,'Pendente','0 - Em Análise','Contrato Automático Do Pedido: '||:NEW.NUMERO,
    TIPOFRETES,V_TIPOENTREGA,TIPOPGTO,TIPOPESSOA,
   -- 'AJUSTE','AJUSTE','AJUSTE','AJUSTE',
-   :new.DTPREVISAO,
+   v_dtmov,
    QTRIGO,
    --'Sem_Padrao',
    'N','0 - A Faturar',to_date(CURRENT_DATE),'N',NRTICKET, P_INSECAOTAXA,P_ESTABORIGEM,
