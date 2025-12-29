9b67e520624564648ec3dfaeb642a9dea0e3ef34
hangelici <henrique.angelici@ourosafra.com.br>
2025-12-29 16:13:19 -0300

    Melhorando performance

--- ID/ft_notapgto.sql
+++ ID/ft_notapgto.sql
@@ -1,120 +1,70 @@
-select distinct
-dados.estab as osestab,
-dados.seqnota as nota,
-dados.ult_dt,
-dados.parcela as qtd_parcela,
-baixa.quitada,
-dados.estab||'#'||dados.seqnota as chave_bx
-
-from(
-
-select
-nfcab.estab,
-nfcab.seqnota,
-max(pag.dtpag)ult_dt,
-max(nroparcela.parcela)parcela
-from nfcab
-
-inner join u_tempresa on u_tempresa.estab = nfcab.estab
-
-left join nfcabagrfin on
-    nfcabagrfin.estab = nfcab.estab
-    and nfcabagrfin.seqnota = nfcab.seqnota
-
-left join agrfinduprec on
-    agrfinduprec.estab = nfcabagrfin.estab
-    and agrfinduprec.seqpagamento = nfcabagrfin.seqpagamento
-
-left join pduprec on
-    pduprec.empresa = agrfinduprec.estab
-    and pduprec.duprec = agrfinduprec.duprec
-
-left join
-    (
-    select
-    empresa,
-    duprec,
-    max(prduprec.dtrecbto)dtpag
-    from prduprec
-    group by
-    empresa,duprec
-    )pag on
-    pag.empresa = pduprec.empresa
-    and pag.duprec = pduprec.duprec
-
-
-left join
-    (
-    select
-    nfcab.estab,
-    nfcab.seqnota,
-    coalesce(max(pduprec.nroparcela),1)parcela
-    from nfcab
-
-    inner join u_tempresa on u_tempresa.estab = nfcab.estab
-
-    left join nfcabagrfin on
-        nfcabagrfin.estab = nfcab.estab
-        and nfcabagrfin.seqnota = nfcab.seqnota
-
-    left join agrfinduprec on
-        agrfinduprec.estab = nfcabagrfin.estab
-        and agrfinduprec.seqpagamento = nfcabagrfin.seqpagamento
-
-    left join pduprec on
-        pduprec.empresa = agrfinduprec.estab
-        and pduprec.duprec = agrfinduprec.duprec
-
-    where
-    nfcab.status <> 'C'
-    and u_tempresa.insumos = 'S'
-    and u_tempresa.exvenda = 'S'
-
-    group by
-    nfcab.estab,
-    nfcab.seqnota 
-    )nroparcela on
-    nroparcela.estab = nfcab.estab
-    and nroparcela.seqnota = nfcab.seqnota
-
-where
-nfcab.status <> 'C'
-and u_tempresa.insumos = 'S'
-and u_tempresa.exvenda = 'S'
-and nfcab.dtemissao >='01/01/2021'
-
-group by
-nfcab.estab,
-nfcab.seqnota
-
-)dados
-
-left join(
-    select 
-    nfcab.estab,
-    nfcab.seqnota,
-    coalesce(nroparcela,1)parcela,
-    pduprec.quitada
-    from nfcab
-
-    inner join u_tempresa on u_tempresa.estab = nfcab.estab
-
-    left join nfcabagrfin on
-        nfcabagrfin.estab = nfcab.estab
-        and nfcabagrfin.seqnota = nfcab.seqnota
-
-    left join agrfinduprec on
-        agrfinduprec.estab = nfcabagrfin.estab
-        and agrfinduprec.seqpagamento = nfcabagrfin.seqpagamento
-
-    left join pduprec on
-        pduprec.empresa = agrfinduprec.estab
-        and pduprec.duprec = agrfinduprec.duprec
-     where
-    nfcab.status <> 'C'
-    and u_tempresa.insumos = 'S'
-    and u_tempresa.exvenda = 'S'
-    )baixa on
-    baixa.estab = dados.estab
-    and baixa.seqnota = dados.seqnota
-    and baixa.parcela = dados.parcela
+with base_nf as (
+   select n.estab,
+          n.seqnota,
+          n.dtemissao
+     from nfcab n
+     join u_tempresa u
+   on u.estab = n.estab
+    where n.status <> 'C'
+      and u.insumos = 'S'
+      and u.exvenda = 'S'
+      and n.dtemissao >= (
+      select dtini
+        from u_periodosafra
+   )
+),fin as (
+   select b.estab,
+          b.seqnota,
+          b.dtemissao,
+          pd.nroparcela,
+          pd.quitada,
+          max(pr.dtrecbto) as dtpag
+     from base_nf b
+     left join nfcabagrfin naf
+   on naf.estab = b.estab
+      and naf.seqnota = b.seqnota
+     left join agrfinduprec af
+   on af.estab = naf.estab
+      and af.seqpagamento = naf.seqpagamento
+     left join pduprec pd
+   on pd.empresa = af.estab
+      and pd.duprec = af.duprec
+     left join prduprec pr
+   on pr.empresa = pd.empresa
+      and pr.duprec = pd.duprec
+    group by b.estab,
+             b.seqnota,
+             pd.nroparcela,
+             pd.quitada,
+             b.dtemissao
+),agregado as (
+   select estab,
+          seqnota,
+          dtemissao,
+          max(dtpag) as ult_dt,
+          coalesce(
+             max(nroparcela),
+             1
+          ) as parcela
+     from fin
+    group by estab,
+             seqnota,
+             dtemissao
+)
+select a.estab as osestab,
+       a.seqnota as nota,
+       a.ult_dt,
+       a.parcela as qtd_parcela,
+       f.quitada,
+       a.estab
+       || '#'
+       || a.seqnota as chave_bx,
+       a.dtemissao
+  from agregado a
+  left join fin f
+on f.estab = a.estab
+   and f.seqnota = a.seqnota
+   and coalesce(
+   f.nroparcela,
+   1
+) = a.parcela
