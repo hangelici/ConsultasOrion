0c5c49c7776ce69c2f37d1aa324b28a8f1ce47f1
hangelici <henrique.angelici@ourosafra.com.br>
2026-03-04 19:36:43 -0300

    Nova versão

--- Cereais/TV/Cereais - Fat. Estoque App.sql
+++ Cereais/TV/Cereais - Fat. Estoque App.sql
@@ -1,57 +1,58 @@
-select
-dados.*,
-case
-	when "Min" between 10 and 15 then 'amarelo'
-	when "Min" > 15 then 'vermelho'
-	else 'verde'
-end cor_status
-
-from(
-
-select
-lcd."Id",
+WITH cargas_filtradas AS (
+SELECT
+    lcd."Id",
+    lcd."Guid",
+    lcd."EmpresaId",
+    lcd."DestinoId",
+    lcd."DataCriacao",
+    lcd."DescrLocalEmbarque",
+    lcd."Descarga"
+FROM "log_CargaDado" lcd
+WHERE
+    lcd."CargaTipoStatusId" <> 7
+    AND lcd."DataCriacao" >= DATE '2025-01-01'
+),
+workflow_atual AS (
+SELECT
+    cf."Guid",
+    sw."WorkflowAcaoId",
+    sw."DataCriacao"
+FROM cargas_filtradas cf
+JOIN LATERAL (
+    SELECT
+        sw."WorkflowAcaoId",
+        sw."DataCriacao"
+    FROM "sys_Workflow" sw
+    WHERE sw."Referencia" = cf."Guid"
+    ORDER BY sw."Id" DESC
+    LIMIT 1
+) sw ON sw."WorkflowAcaoId" IN (52,17)
+)
+SELECT
+cf."Id",
 emp."NomeFantasia" as "Filial",
 si."Descricao" as "Produto",
 lv."Placa",
 acao."Descricao" as "Status",
-lcd."DataCriacao",
-lcd."DescrLocalEmbarque" as "Embarque",
+cf."DataCriacao",
+cf."DescrLocalEmbarque" as "Embarque",
 sem."Nome"||'-'||se."UF" as "Desembarque",
-lcd."Descarga",
-ROUND(EXTRACT(EPOCH from (current_timestamp - flow."DataCriacao")) / 60,0) as "Min",
-ROUND(EXTRACT(EPOCH from (current_timestamp - lcd."DataCriacao")) / 60,0) as "Min_Total"
-
-from "log_CargaDado" lcd
-left join "sys_Pessoa" sp on sp."Id" = lcd."DestinoId"
-left join "sys_EstadoMunicipio" sem on sem."Id" = sp."MunicipioId"
-left join "sys_Estado" se on se."Id" = sem."EstadoId"
-left join "log_CargaTran" lct on lct."CargaDadoId" = lcd."Id"
-left join "log_Veiculo" lv on lv."Id" = lct."VeiculoId"
-left join "log_CargaItem" lci on lci."CargaDadoId" = lcd."Id"
-left join "sys_Item" si on si."Id" = lci."ProdutoId"
-left join "sys_Empresa" emp on emp."Id" = lcd."EmpresaId"
-
-inner join (
-    select
-        sw1."DataCriacao",
-        sw1."WorkflowAcaoId",
-        sw1."Referencia"
-    from (
-        select
-            sw."Referencia",
-            max(sw."Id") as "maxid"
-        from "sys_Workflow" sw
-        group by "Referencia"
-    ) dados
-    inner join "sys_Workflow" sw1 on
-        sw1."Referencia" = dados."Referencia"
-        and sw1."Id" = dados."maxid"
-        and sw1."WorkflowAcaoId" in (52,17)
-) flow on
-    flow."Referencia" = lcd."Guid"
-left join "sys_WorkflowAcao" acao on acao."Id" = flow."WorkflowAcaoId"
-where
-lcd."CargaTipoStatusId" not in (7)
-and extract(year from lcd."DataCriacao") > 2024
-
-) dados
+cf."Descarga",
+(EXTRACT(EPOCH FROM (current_timestamp - wa."DataCriacao"))/60)::int as "Min",
+(EXTRACT(EPOCH FROM (current_timestamp - cf."DataCriacao"))/60)::int as "Min_Total",
+CASE
+    WHEN (EXTRACT(EPOCH FROM (current_timestamp - wa."DataCriacao"))/60) > 15 THEN 'vermelho'
+    WHEN (EXTRACT(EPOCH FROM (current_timestamp - wa."DataCriacao"))/60) >= 10 THEN 'amarelo'
+    ELSE 'verde'
+END cor_status
+FROM cargas_filtradas cf
+JOIN workflow_atual wa ON wa."Guid" = cf."Guid"
+LEFT JOIN "sys_WorkflowAcao" acao ON acao."Id" = wa."WorkflowAcaoId"
+LEFT JOIN "sys_Pessoa" sp ON sp."Id" = cf."DestinoId"
+LEFT JOIN "sys_EstadoMunicipio" sem ON sem."Id" = sp."MunicipioId"
+LEFT JOIN "sys_Estado" se ON se."Id" = sem."EstadoId"
+LEFT JOIN "log_CargaTran" lct ON lct."CargaDadoId" = cf."Id"
+LEFT JOIN "log_Veiculo" lv ON lv."Id" = lct."VeiculoId"
+LEFT JOIN "log_CargaItem" lci ON lci."CargaDadoId" = cf."Id"
+LEFT JOIN "sys_Item" si ON si."Id" = lci."ProdutoId"
+LEFT JOIN "sys_Empresa" emp ON emp."Id" = cf."EmpresaId";
