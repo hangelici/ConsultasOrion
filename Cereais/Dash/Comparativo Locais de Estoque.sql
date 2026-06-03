4d2ee53109b576f8b454874ab63e55508c17c29e
Henrique <henrique.angelici@ourosafra.com.br>
2026-06-03 14:59:07 -0300

    Retirando a função de saldo de item

--- Cereais/Dash/Comparativo Locais de Estoque.sql
+++ Cereais/Dash/Comparativo Locais de Estoque.sql
@@ -1,10 +1,40 @@
+with maxdata as (
+    SELECT
+    i.estab,i.codigosaldo,i.local,i.item,
+    MAX(TO_DATE('01/' || LPAD(COALESCE(i.MES, 1), 2, '0') || '/' || COALESCE(i.ANO, 1), 'DD/MM/YYYY')) AS DATA_MAX
+    from itemsaldo i
+    inner join u_tempresa e on e.estab = i.estab
+    where i.codigosaldo in (:CODIGOSALDO) and e.GRAOS = 'S'
+    AND (TO_DATE('01/' || LPAD(COALESCE(i.MES, 1), 2, '0') || '/' || COALESCE(i.ANO, 1), 'DD/MM/YYYY')) <= :DATA
+    group by i.estab,i.codigosaldo,i.local,i.item
+),
+base as (
+    select
+    t.estab,
+    t.item,
+    itemsaldo.local,
+    sum(itemsaldo.SALDOQUANTIDADE) saldo
+    from itemagroestab t
+    left join itemsaldo on
+        itemsaldo.estab = t.estab
+        and itemsaldo.item = t.item
+    inner join maxdata m on
+        m.estab = itemsaldo.estab
+        and m.item = itemsaldo.ITEM
+        and m.local = itemsaldo.local
+        and m.codigosaldo = itemsaldo.codigosaldo
+        and m.DATA_MAX = TO_DATE('01/' || LPAD(COALESCE(itemsaldo.MES, 1), 2, '0') || '/' || COALESCE(itemsaldo.ANO, 1), 'DD/MM/YYYY')
+    group by t.estab,t.item,itemsaldo.local
+)
 SELECT  FILIAL.ESTAB AS ESTABORIGEM,
         LOCALEST.LOCAL AS LOCALORIGEM,
         LOCEQUI_U.ESTAB AS ESTABDESTINO,
         LOCEQUI_U.LOCAL AS LOCALDESTINO,    
         ITEMAGRO.DESCRICAO||' - '||FILIAL.ESTAB||'-'||LOCALEST.DESCRICAO||' / '||LOCEQUI.ESTAB||'-'||LOCEQUI.DESCRICAO AS LOCALXLOCAL,        
-        SALDOITEM(LOCALEST.ESTAB, ITEMAGROESTAB.ITEM, :CODIGOSALDO, LOCALEST.LOCAL, LOCALEST.LOCAL,:DATA,NULL,NULL,NULL,NULL) AS SALDOORIGEM,
-        SALDOITEM(LOCEQUI_U.ESTAB, ITEMAGROESTAB.ITEM, :CODIGOSALDO, LOCEQUI_U.LOCAL, LOCEQUI_U.LOCAL,:DATA,NULL,NULL,NULL,NULL) AS SALDOEQUIVALENTE
+        nvl(baseori.saldo,0) as SALDOORIGEM,
+        nvl(baseeq.saldo,0) as SALDOEQUIVALENTE
+        --SALDOITEM(LOCALEST.ESTAB, ITEMAGROESTAB.ITEM, :CODIGOSALDO, LOCALEST.LOCAL, LOCALEST.LOCAL,:DATA,NULL,NULL,NULL,NULL) AS SALDOORIGEM,
+        --SALDOITEM(LOCEQUI_U.ESTAB, ITEMAGROESTAB.ITEM, :CODIGOSALDO, LOCEQUI_U.LOCAL, LOCEQUI_U.LOCAL,:DATA,NULL,NULL,NULL,NULL) AS SALDOEQUIVALENTE
 FROM FILIAL
 INNER JOIN LOCALEST
   ON FILIAL.ESTAB = LOCALEST.ESTAB
@@ -24,8 +54,19 @@ INNER JOIN ITEMAGRO
   ON ITEMAGROESTAB.ITEM = ITEMAGRO.ITEM
 LEFT JOIN ITEMGRUPO
   ON ITEMAGRO.GRUPO = ITEMGRUPO.GRUPO
+
+left join base baseori on
+    baseori.estab = LOCALEST.ESTAB
+    and baseori.item = ITEMAGROESTAB.ITEM
+    and baseori.local = LOCALEST.LOCAL
+
+left join base baseeq on
+    baseeq.estab = LOCEQUI_U.ESTAB
+    and baseeq.item = ITEMAGROESTAB.ITEM
+    and baseeq.local = LOCEQUI_U.LOCAL
+
 WHERE 0=0
-  <#if ESTABORIGEM?has_content>
+ <#if ESTABORIGEM?has_content>
     AND FILIAL.ESTAB IN ( :ESTABORIGEM )
   </#if>
   <#if ESTABDESTINO?has_content>
