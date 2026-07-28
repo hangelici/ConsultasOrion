f37dca600e4c69553bd7a62f812f2665482f470b
hangelici <henrique.angelici@ourosafra.com.br>
2026-07-28 16:55:05 -0300

    Ajustes do ticket 1356717

--- Cereais/Procedure/OS_UPD_DESCARGA_387.sql
+++ Cereais/Procedure/OS_UPD_DESCARGA_387.sql
@@ -1,135 +1,195 @@
-create or replace PROCEDURE OS_UPD_DESCARGA_387 AS 
-begin
-for valores in (
-with vinculo as (
+create or replace PROCEDURE OS_UPD_DESCARGA_387 AS
+BEGIN
+
+FOR valores IN (
+
+WITH vinculo AS (
+
     -- ticket 1283092
-    select
-    p.estab,
-    p.seqnota,
-    nf.numerocm,
-    count(p.nfprodutor)
-    from nfcabprodutor p
-    --- notas config 388
-    inner join nfcab nf ON
-        nf.estab = p.estab
-        and nf.seqnota = p.seqnota
-    -- notas config 387
-    inner join nfcab ON
-        nfcab.CHAVEACESSONFE = p.CHAVEACESSONFP
-        and nfcab.estab =  p.estab
-    where nf.notaconf = 388
-    and nfcab.notaconf = 387
-    group BY
-    p.estab,
-    p.seqnota,
-    nf.numerocm
-    having count(p.nfprodutor) = 1
-),
-produtor as (
+    -- ticket 1356717
     SELECT
-    p.estab, p.seqnota, 
-    nf.chaveacessonfe as chave388,
-    p.chaveacessonfp as chave387, 
-    v.numerocm
-    from nfcabprodutor p
-    inner join vinculo v ON
-        v.estab = p.estab
-        and v.seqnota = p.seqnota
-     --- notas config 388
-    inner join nfcab nf ON
-        nf.estab = p.estab
-        and nf.seqnota = p.seqnota
-    -- notas config 387
-    inner join nfcab ON
-        nfcab.CHAVEACESSONFE = p.CHAVEACESSONFP
-        and nfcab.estab =  p.estab
-    where nf.notaconf = 388
-    and nfcab.notaconf = 387
-    and nfcab.dtemissao >= '01/01/2026'
-    and nf.dtemissao >= '01/01/2026'
+        p.estab,
+        p.seqnota,
+        nf.numerocm,
+        COUNT(p.nfprodutor) qtd
+    FROM nfcabprodutor p
+
+    INNER JOIN nfcab nf
+        ON nf.estab = p.estab
+       AND nf.seqnota = p.seqnota
+
+    INNER JOIN nfcab nf_origem
+        ON nf_origem.chaveacessonfe = p.chaveacessonfp
+       AND nf_origem.estab = p.estab
+
+    WHERE (
+            (nf.notaconf = 388  AND nf_origem.notaconf = 387)
+         OR (nf.notaconf = 1388 AND nf_origem.notaconf = 1387)
+          )
+
+    GROUP BY
+        p.estab,
+        p.seqnota,
+        nf.numerocm
+
+    HAVING COUNT(p.nfprodutor) = 1
+
+),
+
+produtor AS (
+
+    SELECT
+        p.estab,
+        p.seqnota,
+        nf.chaveacessonfe      AS chave_destino,
+        p.chaveacessonfp       AS chave_origem,
+        v.numerocm,
+        nf.notaconf            AS notaconf_destino,
+        nf_origem.notaconf     AS notaconf_origem
+
+    FROM nfcabprodutor p
+
+    INNER JOIN vinculo v
+        ON v.estab = p.estab
+       AND v.seqnota = p.seqnota
+
+    INNER JOIN nfcab nf
+        ON nf.estab = p.estab
+       AND nf.seqnota = p.seqnota
+
+    INNER JOIN nfcab nf_origem
+        ON nf_origem.chaveacessonfe = p.chaveacessonfp
+       AND nf_origem.estab = p.estab
+
+    WHERE (
+            (nf.notaconf = 388  AND nf_origem.notaconf = 387)
+         OR (nf.notaconf = 1388 AND nf_origem.notaconf = 1387)
+          )
+
+      AND nf_origem.dtemissao >= DATE '2026-01-01'
+      AND nf.dtemissao >= DATE '2026-01-01'
+
 )
-    select 
-    produtor.CHAVE387,
+
+SELECT
+    produtor.chave_origem AS chave387,
     u.estab,
     u.data,
     u.pliquido,
     u.retencao,
     u.dt_inclusao,
-    produtor.chave388,
-    u388.data as data388,
-    nvl(u388.pliquido,0) as pliquido388,
-    nvl(u388.retencao,0) as retencao388,
-    case when u.data is null then u388.data else u.data end data_nova,
-    case when (u.pliquido is null or u.pliquido = 0) then nvl(u388.pliquido,0) else nvl(u.pliquido,0) end pliq_novo,
-    case when (u.retencao is null or u.retencao = 0) then nvl(u388.retencao,0) else nvl(u.retencao,0) end ret_novo,
-    case 
-        when u.data is null or (u.pliquido is null or u.pliquido = 0) or (u.retencao is null or u.retencao = 0) then trunc(current_date)
-        else u.dt_inclusao
-    end dtinclusao_nova
-    from U_DESCARGA_TRADING u
-    inner join produtor ON
-        produtor.CHAVE387 = u.chaveacesso
-        and produtor.estab = u.estab
-    inner join U_DESCARGA_TRADING u388 on
-        u388.chaveacesso = produtor.chave388
-        and u388.estab = produtor.estab
-    where
-    (u.data is NULL
-    OR
-    (u.pliquido is null or u.pliquido = 0)
-    OR
-    (u.retencao is null or u.retencao = 0))
-    and u.dt_inclusao is not NULL
-    and nvl(u.status,'0') <> '23'
-    and not exists (
-        select 1
-        from U_DESCARGA_TRADING x
-        where x.estab = produtor.estab
-        and x.chaveacesso = produtor.chave388
-        group by x.estab, x.chaveacesso
-        having count(*) > 1
-    )
-)
-loop
-update u_descarga_trading d set
-    d.PLIQUIDO = valores.PLIQ_NOVO,
-    d.DATA = valores.DATA_NOVA,
-    d.RETENCAO = valores.RET_NOVO,
-    d.dt_inclusao = valores.dtinclusao_nova
-where d.chaveacesso = valores.CHAVE387
-and d.estab = valores.estab;
 
-insert into u_log_upd_387 (
-    chave387 ,
-    estab,
-    data,
-    pliquido ,
-    retencao  ,
-    chave388,
-    data388 ,
-    pliquido388 ,
-    retencao388 ,
-    data_nova ,
-    pliq_novo ,
-    ret_novo,
-    dt_inclusao,
-    dt_inclusao_nova
-    ) values (
-        valores.chave387 ,
+    produtor.chave_destino AS chave388,
+
+    u388.data AS data388,
+    NVL(u388.pliquido,0) AS pliquido388,
+    NVL(u388.retencao,0) AS retencao388,
+
+    CASE
+        WHEN u.data IS NULL THEN u388.data
+        ELSE u.data
+    END AS data_nova,
+
+    CASE
+        WHEN (u.pliquido IS NULL OR u.pliquido = 0)
+            THEN NVL(u388.pliquido,0)
+        ELSE NVL(u.pliquido,0)
+    END AS pliq_novo,
+
+    CASE
+        WHEN (u.retencao IS NULL OR u.retencao = 0)
+            THEN NVL(u388.retencao,0)
+        ELSE NVL(u.retencao,0)
+    END AS ret_novo,
+
+    CASE
+        WHEN u.data IS NULL
+          OR (u.pliquido IS NULL OR u.pliquido = 0)
+          OR (u.retencao IS NULL OR u.retencao = 0)
+        THEN TRUNC(CURRENT_DATE)
+        ELSE u.dt_inclusao
+    END AS dtinclusao_nova
+
+FROM u_descarga_trading u
+
+INNER JOIN produtor
+    ON produtor.chave_origem = u.chaveacesso
+   AND produtor.estab = u.estab
+
+INNER JOIN u_descarga_trading u388
+    ON u388.chaveacesso = produtor.chave_destino
+   AND u388.estab = produtor.estab
+
+WHERE
+(
+    u.data IS NULL
+ OR u.pliquido IS NULL
+ OR u.pliquido = 0
+ OR u.retencao IS NULL
+ OR u.retencao = 0
+)
+
+AND u.dt_inclusao IS NOT NULL
+AND NVL(u.status,'0') <> '23'
+
+AND NOT EXISTS (
+    SELECT 1
+    FROM u_descarga_trading x
+    WHERE x.estab = produtor.estab
+      AND x.chaveacesso = produtor.chave_destino
+    GROUP BY
+        x.estab,
+        x.chaveacesso
+    HAVING COUNT(*) > 1
+)
+
+)
+
+LOOP
+
+    UPDATE u_descarga_trading d
+       SET d.pliquido    = valores.pliq_novo,
+           d.data         = valores.data_nova,
+           d.retencao     = valores.ret_novo,
+           d.dt_inclusao  = valores.dtinclusao_nova
+     WHERE d.chaveacesso = valores.chave387
+       AND d.estab = valores.estab;
+
+    INSERT INTO u_log_upd_387 (
+        chave387,
+        estab,
+        data,
+        pliquido,
+        retencao,
+        chave388,
+        data388,
+        pliquido388,
+        retencao388,
+        data_nova,
+        pliq_novo,
+        ret_novo,
+        dt_inclusao,
+        dt_inclusao_nova
+    )
+    VALUES (
+        valores.chave387,
         valores.estab,
         valores.data,
-        valores.pliquido ,
-        valores.retencao  ,
+        valores.pliquido,
+        valores.retencao,
         valores.chave388,
-        valores.data388 ,
-        valores.pliquido388 ,
-        valores.retencao388 ,
-        valores.data_nova ,
-        valores.pliq_novo ,
+        valores.data388,
+        valores.pliquido388,
+        valores.retencao388,
+        valores.data_nova,
+        valores.pliq_novo,
         valores.ret_novo,
         valores.dt_inclusao,
         valores.dtinclusao_nova
     );
-end loop;
-commit;
-end;
+
+END LOOP;
+
+COMMIT;
+
+END;
