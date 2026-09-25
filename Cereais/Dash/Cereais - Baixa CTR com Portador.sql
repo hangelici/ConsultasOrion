1bf13bb230993dcf5f9d3117b6aea8c0c19ce147
hangelici <henrique.angelici@ourosafra.com.br>
2026-09-25 11:34:06 -0300

    Adicionando contratos de compra

--- Cereais/Dash/Cereais - Baixa CTR com Portador.sql
+++ Cereais/Dash/Cereais - Baixa CTR com Portador.sql
@@ -6,12 +6,14 @@ with base as (
     c.estabnota,
     c.seqnota,
     nfcab.nota,
+    contrato.numerocm,
     nfcabagrfin.seqpagamento,
     sum(c.VALOR) as VALOR
     from contratonfite c
     inner join contrato on
         contrato.estab = c.estab
         and contrato.contrato = c.contrato
+    inner join contratocfg on contratocfg.contconf = contrato.contconf
     inner join filial on filial.estab = c.estab
     inner join cidade on cidade.cidade = filial.cidade
     inner join nfcab on
@@ -25,6 +27,7 @@ with base as (
     and contrato.dtemissao between :DTINI and :DTFIM
     and (0 IN (:NUMEROCM) OR contrato.numerocm in (:NUMEROCM))
     and ('X' in (:UF) OR CIDADE.UF IN (:UF))
+    and (contratocfg.entradasaida = 'S' OR (contrato.estab = 79 and contratocfg.entradasaida = 'E'))
     group by
     c.estab,
     c.contrato,
@@ -32,7 +35,7 @@ with base as (
     c.seqnota,
     nfcab.nota,
     nfcabagrfin.seqpagamento,
-    filial.reduzido
+    filial.reduzido, contrato.numerocm
 ),
 base_rec as (
     select /*+ MATERIALIZE */
@@ -49,13 +52,127 @@ base_rec as (
         where base.estab = agrfinduprec.estab
         and base.seqpagamento = agrfinduprec.seqpagamento
     )
+),
+base_pag as (
+    select /*+ MATERIALIZE */
+    u.empresa,
+    u.duppag,
+    u.fornecedor,
+    u.seqpagtodu,
+    g.seqpagamento,
+    u.estabfornecedor
+    from PPDUPPAG u
+    inner join AGRFINDUPPAG g on
+        g.estab = u.empresa
+        and g.duppag = u.duppag
+        and g.fornecedor = u.fornecedor
+        and g.estabfornecedor = u.estabfornecedor
+    where exists  (
+        select 1 from base
+        where base.estab = g.estab
+        and base.seqpagamento = g.seqpagamento
+        and base.numerocm = g.fornecedor
+    )
 )
+select
+b.reduzido||'-'||bp.empresa as empresa,
+b.contrato,
+bp.duppag,
+b.nota,
+contamov.nome||'-'||contamov.numerocm as cliente,
+b.valor as vlrctr,
+pd.valor as vlrdup,
+sum(CASE WHEN pportado.descricao like '%BANCO%' or pportado.descricao like '%CAIXA%' then ppduppad.VALOR else 0 END) as banco,
+SUM(CASE 
+    WHEN NVL(PLANCA.PORTADOR,0) <> 101 
+        AND pportado.descricao not like '%BANCO%' 
+        AND pportado.descricao not like '%CAIXA%'
+    THEN ppduppad.VALOR ELSE 0 END
+) AS OUTROS_PORTADORES,
+SUM(CASE WHEN NVL(PLANCA.PORTADOR,0) = 101 THEN ppduppad.VALOR ELSE 0 END) AS ACERTO,
+SUM( nvl(ppaduche.VLRCHEQUEP,0) + nvl(ppaduchr.VLRCHEQUE,0)  ) AS CHEQUE,
+0 AS OUTROS,
+0 AS CARTAO,
+SUM(ppaducm.VALOR) AS CONTAMOV,
+SUM(ppduppadup.VALOR) AS DINHEIRO
+from base_pag bp
+
+inner join contamov on contamov.numerocm = bp.fornecedor
+
+inner join PDUPPAGA pd on
+    pd.empresa = bp.empresa
+    and pd.duppag = bp.duppag
+    and pd.fornecedor = bp.fornecedor
+    and pd.estabfornecedor = bp.estabfornecedor
+
+inner join base b on
+    b.estab = bp.empresa
+    and b.seqpagamento = bp.seqpagamento
+    and b.numerocm = bp.fornecedor
+
+left join ppduppad
+    on (ppduppad.empresa = bp.empresa)
+    and (ppduppad.fornecedor = bp.fornecedor)
+    and (ppduppad.estabfornecedor = bp.estabfornecedor)
+    and (ppduppad.duppag = bp.duppag)
+    and (ppduppad.seqpagtodu = bp.seqpagtodu)
+
+left join planca on 
+    (ppduppad.empresa = planca.empresa)
+    and (ppduppad.dtlanca = planca.dtlanca)
+    and (ppduppad.seqlanca = planca.seqlanca)
+
+left join pportado on 
+    planca.empresa = pportado.empresa
+    and planca.portador = pportado.portador
+
+left join ppaducm on 
+    (ppaducm.empresa = bp.empresa)
+    and (ppaducm.fornecedor = bp.fornecedor)
+    and (ppaducm.estabfornecedor = bp.estabfornecedor)
+    and (ppaducm.duppag = bp.duppag)
+    and (ppaducm.seqpagtodu = bp.seqpagtodu)
+
+left join ppduppadup on  
+    (ppduppadup.empresa         = bp.empresa)
+    and (ppduppadup.fornecedor         = bp.fornecedor)
+    and (ppduppadup.estabfornecedor = bp.estabfornecedor)
+    and (ppduppadup.duppag             = bp.duppag)
+    and (ppduppadup.seqpagtodu         = bp.seqpagtodu)
+
+left join ppaduche on 
+    (ppaduche.empresa = bp.empresa)
+    and (ppaduche.fornecedor = bp.fornecedor)
+    and (ppaduche.estabfornecedor = bp.estabfornecedor)
+    and (ppaduche.duppag = bp.duppag)
+    and (ppaduche.seqpagtodu = bp.seqpagtodu)
+
+left join ppaduchr on 
+    (ppaduchr.empresa = bp.empresa)
+    and (ppaduchr.fornecedor = bp.fornecedor)
+    and (ppaduchr.estabfornecedor = bp.estabfornecedor)
+    and (ppaduchr.duppag = bp.duppag)
+    and (ppaduchr.seqpagtodu = bp.seqpagtodu)
+
+group by
+b.reduzido,
+bp.empresa,
+b.contrato,
+bp.duppag,
+b.nota,
+contamov.nome,
+contamov.numerocm,
+b.valor,
+pd.valor
+
+union all
+
+
 select
 b.reduzido||'-'||br.empresa as empresa,
 b.contrato,
 br.duprec,
 b.nota,
---br.seqpagamento,
 contamov.nome||'-'||contamov.numerocm as cliente,
 b.valor as vlrctr,
 pduprec.valor as vlrdup,
@@ -70,7 +187,8 @@ SUM(CASE WHEN NVL(PLANCA.PORTADOR,0) = 101 THEN PRDUPRED.VALOR ELSE 0 END) AS AC
 SUM(PRDURECH.VLRCHEQREC) AS CHEQUE,
 SUM(PRDUREOUT.VALOR) AS OUTROS,
 SUM(PRDURECAR.VALOR) AS CARTAO,
-SUM(PRDURECM.VALOR) AS CONTAMOV
+SUM(PRDURECM.VALOR) AS CONTAMOV,
+SUM(prduredup.VALOR) AS DINHEIRO
 from base_rec br
 
 inner join pduprec on
@@ -116,6 +234,11 @@ left join prdurecm on (prdurecm.empresa = br.empresa)
 left join prdurecar on (prdurecar.empresa = br.empresa)
   and (prdurecar.duprec = br.duprec)
   and (prdurecar.seqrecbto = br.seqrecbto)
+
+left join prduredup on 
+    (prduredup.empresa = br.empresa)
+    and (prduredup.duprec = br.duprec)
+    and (prduredup.seqrecbto = br.seqrecbto)
 
 group by
 br.empresa,
